306af2305fffe46e922e4a398d072d789605bdd0
AzizAlbaqshi <eng.albaqshi@gmail.com>
2021-06-08 15:32:54 +0300

    Created home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import posters from "./posters";
+
 
 function App() {
+  const posterList = posters.map((poster) => (
+    <div className="poster" key={poster.id}>
+      <img alt={poster.name} className="poster-image" src={poster.image} />
+      <p className="text">{poster.name}</p>
+      <p className="text">{poster.price} KD</p>
+    </div>
+  ));
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <div>
+     <h1 id="welcome" >Welcome to our Posters Shop</h1>
+     <h4 id="qout"> Where Stolen ARTS and WALLS combaind !! </h4>
+    
+    <img 
+    id="primary-img" 
+    src="https://helmsbakerydistrict.com/wp-content/uploads/2019/10/Bauhaus_Posters-1.jpg"
+     alt="storeimg" />
+    </div>
+    <div className="list">{posterList}</div>
     </div>
   );
 }
---
+++ src/posters.js
@@ -0,0 +1,36 @@
+
+const posters = [
+
+
+{
+    id: 1,
+    name: "Iron Man poster",
+    price: 12,
+    image:
+      "https://i1.wp.com/www.infamousinspiration.com/wp-content/uploads/2017/06/Tony-Stark-Iron-Man-Poster-2.jpg",
+  },
+  {
+    id: 2,
+    name: "Hulk poster",
+    price: 12,
+    image:
+      "https://i1.wp.com/www.infamousinspiration.com/wp-content/uploads/2017/06/Incredible-Hulk-Poster-2.jpg",
+  },
+  {
+    id: 3,
+    name: "captain america poster",
+    price: 12,
+    image:
+      "https://i1.wp.com/www.infamousinspiration.com/wp-content/uploads/2017/06/The-First-Avenger-Captain-America-Poster-2.jpg",
+  },
+  {
+    id: 4,
+    name: "Thor poster",
+    price: 12,
+    image: "https://i1.wp.com/www.infamousinspiration.com/wp-content/uploads/2017/06/Thor-Poster-2.jpg",
+  },
+];
+
+export default posters;
+
+
